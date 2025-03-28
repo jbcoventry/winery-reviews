9a7655cea99f3943c9b22ac6b724986e8590759e
Jayden <69566911+jbcoventry@users.noreply.github.com>
2025-03-28 13:52:50 +1100

    last updated string format improved

--- src/pages/ComparisonPage/LastUpdated.tsx
+++ src/pages/ComparisonPage/LastUpdated.tsx
@@ -4,11 +4,15 @@ type LastUpdatedProps = {
 
 const LastUpdated = ({ timestamp }: LastUpdatedProps) => {
   return (
-    <div className="text-xs md:text-base">
+    <div className="text-xs md:my-2 md:text-base">
       Reviews Updated:
       {typeof timestamp === "string"
-        ? " " + new Date(timestamp).toLocaleString()
-        : "N/A"}
+        ? " " +
+          new Date(timestamp).toLocaleString("en-GB", {
+            timeZoneName: "shortGeneric",
+            hour12: true,
+          })
+        : " N/A"}
     </div>
   );
 };
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,7 +5,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 export const Route = createRootRoute({
   component: () => (
     <>
-      <header className="p-4 text-center text-2xl">
+      <header className="p-4 text-center text-2xl md:text-3xl">
         <Link to="/">
           <p className="inline">Hunter Valley Wineries</p>
         </Link>
